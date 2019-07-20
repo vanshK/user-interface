65765e175db8c674c4bfdb5b232a4dc66ce57c3f
vkochar <vansh_kochar@sfu.ca>
2019-07-20 16:16:37 -0700

    Update paper-dashboard-angular-master/src/app/pages/meetings/meetings.component.ts, paper-dashboard-angular-master/src/app/pages/meetings/meetings.component.html files

---
+++ paper-dashboard-angular-master/src/app/pages/meetings/meetings.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meeting-cmp',
+  moduleId: module.id,
+  templateUrl: 'meetings.component.html',
+})
+
+export class MeetingsComponent implements OnInit{
+    ngOnInit(){
+    }
+}
